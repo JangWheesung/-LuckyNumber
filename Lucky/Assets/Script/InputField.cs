d92866e2e2e1244f66f4d10ebdf441587caae494
JangWheesung <gameplay1qaz@gmail.com>
2023-04-12 18:51:05 +0900

    ChinmachineShake

--- Lucky/Assets/Script/InputField.cs
+++ Lucky/Assets/Script/InputField.cs
@@ -1,45 +1,52 @@
+using Cinemachine;
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using FD.Dev;
 
 public class InputField : MonoBehaviour
 {
     [SerializeField] private Text inputFieldText;
-    [SerializeField] private Text expFieldText;
     [SerializeField] private Text maxFieldText;
 
     [SerializeField] private Text randomText;
     [SerializeField] private Text randomArray;
 
+    [SerializeField] private float crash = 2.5f;
+
     private GameObject[] arrayCard;
+    private CinemachineVirtualCamera cv;
+    private CinemachineBasicMultiChannelPerlin noise;
 
     public void RandomNumber()
     {
-        int r;
-        int num;
-        while (true)
+        cv = FindObjectOfType<CinemachineVirtualCamera>();
+        noise = cv.GetCinemachineComponent<CinemachineBasicMultiChannelPerlin>();
+        noise.m_AmplitudeGain = crash;
+
+        try
         {
-            r = Random.Range(1, int.Parse(inputFieldText.text));
-            if (expFieldText.text == "")
-                break;
-            else if (r != int.Parse(expFieldText.text))
-                break;
+            FAED.InvokeDelay(() => 
+            {
+                noise.m_AmplitudeGain = 0.0f;
+
+                randomText.text = UnityEngine.Random.Range(1,
+                int.Parse(inputFieldText.text) + 1).ToString();
+            }, 0.1f);
         }
-        randomText.text = Random.Range(0, r).ToString();
+        catch (Exception exp) { }
     }
 
     public void RandomArray()
     {
+        InputField d;
+
         int[] r = new int[99];
         for (int i = 0; i < int.Parse(maxFieldText.text); i++)
         {
-            while (true)
-            {
-                r[i] = Random.Range(1, int.Parse(inputFieldText.text));
-                if (r[i] != int.Parse(expFieldText.text))
-                    break;
-            }
+            r[i] = UnityEngine.Random.Range(1, int.Parse(inputFieldText.text));
         }
 
         arrayCard = GameObject.FindGameObjectsWithTag("Card");
