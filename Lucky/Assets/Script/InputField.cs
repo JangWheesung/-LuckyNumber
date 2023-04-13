c5d093b845d66671aea35fb758d659f86f530ce1
JangWheesung <gameplay1qaz@gmail.com>
2023-04-13 23:38:11 +0900

    GameClear

--- Lucky/Assets/Script/InputField.cs
+++ Lucky/Assets/Script/InputField.cs
@@ -5,58 +5,107 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using FD.Dev;
+using DG.Tweening;
 
 public class InputField : MonoBehaviour
 {
+    [Header("RandomNumber")]
     [SerializeField] private Text inputFieldText;
-    [SerializeField] private Text maxFieldText;
 
+    [Header("RandomArray")]
     [SerializeField] private Text randomText;
-    [SerializeField] private Text randomArray;
+    [SerializeField] private Text maxFieldText;
+    [SerializeField] private GameObject card;
+    [SerializeField] private GameObject panel;
 
+    [Header("Chinmashine")]
     [SerializeField] private float crash = 2.5f;
 
     private GameObject[] arrayCard;
     private CinemachineVirtualCamera cv;
     private CinemachineBasicMultiChannelPerlin noise;
 
-    public void RandomNumber()
+    bool isSetting;
+
+    private void Awake()
     {
         cv = FindObjectOfType<CinemachineVirtualCamera>();
         noise = cv.GetCinemachineComponent<CinemachineBasicMultiChannelPerlin>();
-        noise.m_AmplitudeGain = crash;
-
-        try
-        {
-            FAED.InvokeDelay(() => 
-            {
-                noise.m_AmplitudeGain = 0.0f;
-
-                randomText.text = UnityEngine.Random.Range(1,
-                int.Parse(inputFieldText.text) + 1).ToString();
-            }, 0.1f);
-        }
-        catch (Exception exp) { }
     }
 
     public void RandomArray()
     {
-        InputField d;
-
         int[] r = new int[99];
-        for (int i = 0; i < int.Parse(maxFieldText.text); i++)
+        List<int> wt = new List<int>();
+
+        InputTry();
+        CardDestory();
+        CardNumber(out r, out wt);
+        StartCoroutine(CardSettingDelay(r, 0.1f));
+
+        FAED.InvokeDelay(() => { isSetting = false; }, int.Parse(maxFieldText.text) * 0.1f);
+    }
+
+    void InputTry()
+    {
+        if (randomText.text == "" || maxFieldText.text == "" || isSetting)
+            return;
+        isSetting = true;
+    }
+
+    void CardDestory()
+    {
+        foreach (GameObject card in GameObject.FindGameObjectsWithTag("Card"))
         {
-            r[i] = UnityEngine.Random.Range(1, int.Parse(inputFieldText.text));
+            Destroy(card);
         }
+    }
 
-        arrayCard = GameObject.FindGameObjectsWithTag("Card");
-        foreach(GameObject card in arrayCard)
-            card.SetActive(false);
+    void CardNumber(out int[] r, out List<int> wt)
+    {
+        r = new int[99];
+        wt = new List<int>();
+        wt.Clear();
 
         for (int i = 0; i < int.Parse(maxFieldText.text); i++)
         {
-            arrayCard[i].SetActive(true);
-            arrayCard[i].transform.GetChild(0).GetComponent<Text>().text = r.ToString();
+            if (i - int.Parse(randomText.text) >= 0)
+                wt.Clear();
+
+            bool isDuplicate = true;
+            while (isDuplicate)
+            {
+                r[i] = UnityEngine.Random.Range(1, int.Parse(randomText.text) + 1);
+                isDuplicate = wt.Contains(r[i]);
+            }
+            wt.Add(r[i]);
+        }
+    }
+
+    void CardSetting(GameObject card, int[] r, int i)
+    {
+        card.transform.SetParent(panel.transform);
+        card.transform.localScale = new Vector3(1.8f, 1.8f, 1.8f);
+
+        card.transform.GetChild(0).GetComponent<Image>().DOFade(1, 0.1f);
+        card.transform.GetChild(0).GetChild(0).GetComponent<Text>()
+            .text = r[i].ToString();
+    }
+
+    void CarmerShake()
+    {
+        noise.m_AmplitudeGain = crash;
+        FAED.InvokeDelay(() => { noise.m_AmplitudeGain = 0; }, 0.1f);
+    }
+
+    IEnumerator CardSettingDelay(int[] r, float time)
+    {
+        for (int i = 0; i < int.Parse(maxFieldText.text); i++)
+        {
+            GameObject creatCard = Instantiate(card);
+            CardSetting(creatCard, r, i);
+            CarmerShake();
+            yield return new WaitForSeconds(time);
         }
     }
 }
